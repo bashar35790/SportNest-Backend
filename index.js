ba87df579ace9360d5911f9a33c0b5e0c0d40723
Abul Bashar <bashar35790@gmail.com>
2026-05-19 10:22:06 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI;
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res)=>{
-    res.send("Hello Server!");
-})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Send a ping to confirm a successful connection
+    const database = await client.db("sportnest").command({ ping: 1 });
+    const sportsCollection = database.collection("sports");
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Server!");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
